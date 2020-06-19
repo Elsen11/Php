68c8196c611994bf05f1aac8eb0019fd8e11a813
elsenelvansen <elsenelvansen@gmail.com>
2020-06-19 11:05:54 +0700

    learn

--- Assorted Project/LearnAPI/API/posts/update.php
+++ Assorted Project/LearnAPI/API/posts/update.php
@@ -14,38 +14,21 @@ $db = $database->connect();
 // instanciate blog post object
 $post = new Post($db);
 
-$data = json_decode(file_get_contents("php://input"));
-
-$post->title = $data->title;
-$post->body = $data->body;
-$post->author = $data->author;
-$post->category_id = $data->category_id;
-
-// Create post
-if ($post->create()) {
-    echo json_encode(
-        array('message' => 'Post Created')
-    );
-} else {
-    echo json_encode(
-        array('message' => 'Post Not Created')
-    );
-}
 //get raw posted data
-/* $data = json_decode(file_get_contents("php://input"));
+$data = json_decode(file_get_contents('php://input'));
 
+$post->id = $data->id;
 $post->title = $data->title;
 $post->body = $data->body;
 $post->author = $data->author;
 $post->category_id = $data->category_id;
 
-if ($post->create()) {
+if ($post->update()) {
     echo json_encode(
-        array('message' => 'post Created')
+        array('message' => 'post Updated')
     );
 } else {
     echo json_encode(
-        array('message' => 'post NOT Created')
+        array('message' => 'post NOT Updated')
     );
 }
- */
--- Assorted Project/LearnAPI/models/Post.php
+++ Assorted Project/LearnAPI/models/Post.php
@@ -72,14 +72,16 @@ class Post
     public function create()
     {
         //create query
-        $query = ' INSERT INTO 
+        $query = 'INSERT INTO 
         ' . $this->table . '
         SET 
-            title= :title,
-            body=   :body,
-            author= :author,
+            title =  :title,
+            body =   :body,
+            author = :author,
             category_id = :category_id';
+
         $stmt = $this->conn->prepare($query);
+
         $this->title = htmlspecialchars(strip_tags($this->title));
         $this->body = htmlspecialchars(strip_tags($this->body));
         $this->author = htmlspecialchars(strip_tags($this->author));
@@ -90,7 +92,61 @@ class Post
         $stmt->bindParam(':author', $this->author);
         $stmt->bindParam(':category_id', $this->category_id);
 
-        if ($stmt->exeute()) {
+        if ($stmt->execute()) {
+            print("success");
+            return true;
+        }
+        printf("ERROR : %s.\n", $stmt->error);
+        return false;
+    }
+
+    public function update()
+    {
+        //create query
+        $query = ' UPDATE  
+        ' . $this->table . '
+        SET 
+            title= :title,
+            body=   :body,
+            author= :author,
+            category_id = :category_id
+        WHERE
+        id = :id';
+
+        $stmt = $this->conn->prepare($query);
+
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->body = htmlspecialchars(strip_tags($this->body));
+        $this->author = htmlspecialchars(strip_tags($this->author));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        $stmt->bindParam(':title', $this->title);
+        $stmt->bindParam(':body', $this->body);
+        $stmt->bindParam(':author', $this->author);
+        $stmt->bindParam(':category_id', $this->category_id);
+        $stmt->bindParam(':id', $this->id);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        printf("ERROR : %s.\n", $stmt->error);
+        return false;
+    }
+
+    public function delete()
+    {
+        $query = 'DELETE FROM ' . $this->table . ' 
+        WHERE 
+            id = :id';
+
+        $stmt = $this->conn->prepare($query);
+
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        $stmt->bindParam(':id', $this->id);
+
+        if ($stmt->execute()) {
             return true;
         }
         printf("ERROR : %s.\n", $stmt->error);
